41da723262be640048aa5f852a2f572d4d3f52c8
Eli <eliaarongilbert@gmail.com>
2022-09-01 13:45:02 +0100

    Day 2 - learning about json too

--- frankenstein/lib/main.dart
+++ frankenstein/lib/main.dart
@@ -1,115 +1,442 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
+Future<void> readJson() async {
+  final String response = await rootBundle.loadString('SRDspells.json');
+  final data = await json.decode(response);
+  final users = data['users'];
+  print(users);
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Spell {
+  final String name;
+  final String effect;
+  final int level;
+  final String? verbal;
+  final String? somatic;
+  final String? material;
+  Spell(
+      {required this.name,
+      required this.level,
+      required this.effect,
+      this.material,
+      this.somatic,
+      this.verbal});
+}
 
-  // This widget is the root of your application.
+const pageLinker = {
+  0: MainMenu(),
+  1: CreateACharacter(),
+  2: SearchForContent(),
+  3: MyCharacters(),
+  4: RollDice(),
+  5: CustomContent()
+};
+void main() => runApp(const Homepage());
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+  static const String _title = 'Frankenstein\'s - a D&D 5e character builder';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+              icon: const Icon(Icons.image),
+              tooltip: 'Put logo here',
+              onPressed: () {}),
+          title: const Center(child: Text(_title)),
+          actions: <Widget>[
+            IconButton(
+                icon: const Icon(Icons.settings),
+                tooltip: 'settings??',
+                onPressed: () {}),
+          ],
+        ),
+        //appBar: AppBar(title: const Text(_title)),
+
+        //appBar: AppBar(title: new Center(child: const Text(_title))),
+        body: const MainMenu(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class ScreenTop extends StatelessWidget {
+  final int? pagechoice;
+  const ScreenTop({Key? key, this.pagechoice}) : super(key: key);
+  static const String _title = 'Frankenstein\'s - a D&D 5e character builder';
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+              icon: const Icon(Icons.home),
+              tooltip: 'Return to the main menu',
+              onPressed: () {
+                if (pagechoice != 0) {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              const ScreenTop(pagechoice: 0)));
+                } else {
+                  ////SHOW ISSUE LATER IF TIME
+                }
+              }),
+          title: const Center(child: Text(_title)),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.arrow_back),
+              tooltip: 'Return to the previous page',
+              onPressed: () {
+                Navigator.pop(context);
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            IconButton(
+                icon: const Icon(Icons.settings),
+                tooltip: 'Settings??',
+                onPressed: () {}),
+          ],
+        ),
+        //pick relevent call
+
+        body: pageLinker[pagechoice],
+      ),
+    );
+  }
+}
+
+class MainMenu extends StatelessWidget {
+  const MainMenu({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.blue,
+                child: Text(
+                  textAlign: TextAlign.center,
+                  'Main Menu',
+                  style: TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white),
+                ),
+              ),
             ),
           ],
         ),
+        const SizedBox(height: 100),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: EdgeInsets.fromLTRB(55, 25, 55, 25),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                side: BorderSide(
+                    width: 5, color: Color.fromARGB(255, 7, 26, 239)),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ScreenTop(pagechoice: 1)),
+                );
+              },
+              child: Text(
+                textAlign: TextAlign.center,
+                'Create a \ncharacter',
+                style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+              ),
+            ),
+            const SizedBox(width: 100),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: EdgeInsets.fromLTRB(55, 25, 55, 25),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                side: BorderSide(
+                    width: 5, color: Color.fromARGB(255, 7, 26, 239)),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ScreenTop(pagechoice: 2)),
+                );
+              },
+              child: Text(
+                textAlign: TextAlign.center,
+                'Search for\ncontent',
+                style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+              ),
+            ),
+            const SizedBox(width: 100),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: EdgeInsets.fromLTRB(55, 25, 55, 25),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                side: BorderSide(
+                    width: 5, color: Color.fromARGB(255, 7, 26, 239)),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ScreenTop(pagechoice: 3)),
+                );
+              },
+              child: Text(
+                textAlign: TextAlign.center,
+                'My \ncharacters',
+                style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 100),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: EdgeInsets.fromLTRB(30, 42, 30, 42),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                side: BorderSide(
+                    width: 5, color: Color.fromARGB(255, 7, 26, 239)),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ScreenTop(pagechoice: 4)),
+                );
+              },
+              child: Text(
+                'Roll dice',
+                style: TextStyle(
+                    fontSize: 45,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+              ),
+            ),
+            const SizedBox(width: 100),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: EdgeInsets.fromLTRB(55, 25, 55, 25),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                side: BorderSide(
+                    width: 5, color: Color.fromARGB(255, 7, 26, 239)),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ScreenTop(pagechoice: 5)),
+                );
+              },
+              child: Text(
+                textAlign: TextAlign.center,
+                'Custom \ncontent',
+                style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class CreateACharacter extends StatelessWidget {
+  const CreateACharacter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: DefaultTabController(
+      length: 10,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              textAlign: TextAlign.center,
+              'Create a character',
+              style: TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white),
+            ),
+          ),
+          bottom: const TabBar(
+            tabs: [
+              Tab(child: Text("Basics")),
+              Tab(child: Text("Race")),
+              Tab(child: Text("Class")),
+              Tab(child: Text("Backround")),
+              Tab(child: Text("Ability scores")),
+              Tab(child: Text("Spells")),
+              Tab(child: Text("Equipment")),
+              Tab(child: Text("Boons and magic items")),
+              Tab(child: Text("Backstory")),
+              Tab(child: Text("Finishing up")),
+            ],
+          ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ));
+  }
+}
+
+class SearchForContent extends StatelessWidget {
+  const SearchForContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.blue,
+                child: Text(
+                  textAlign: TextAlign.center,
+                  'Search for content',
+                  style: TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class MyCharacters extends StatelessWidget {
+  const MyCharacters({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.blue,
+                child: Text(
+                  textAlign: TextAlign.center,
+                  'My Characters',
+                  style: TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class RollDice extends StatelessWidget {
+  const RollDice({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.blue,
+                child: Text(
+                  textAlign: TextAlign.center,
+                  'Roll dice',
+                  style: TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class CustomContent extends StatelessWidget {
+  const CustomContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.blue,
+                child: Text(
+                  textAlign: TextAlign.center,
+                  'Custom content',
+                  style: TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
