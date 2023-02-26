b4995459939957e54aa6703bdf911e4402e72040
Eli <eliaarongilbert@gmail.com>
2023-02-26 23:38:40 +0000

    new project location as one drive ruined the other one

---
+++ android/app/src/main/kotlin/com/example/frankenstein/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.frankenstein
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/PDFdocs/preview_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+//import 'package:makepdfs/models/invoice.dart';
+//import 'package:makepdfs/pages/pdfexport/pdf/pdfexport.dart';
+import 'package:frankenstein/PDFdocs/pdf_final_display.dart';
+//import 'package:printing/printing.dart';
+import 'package:frankenstein/PDFdocs/character_class.dart';
+
+//import 'package:frankenstein/PDFdocs/preview_page.dart';
+class DetailPage extends StatelessWidget {
+  final Character invoice;
+  const DetailPage({
+    Key? key,
+    required this.invoice,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => PdfPreviewPage(invoice: invoice),
+            ),
+          );
+          // rootBundle.
+        },
+        child: const Icon(Icons.picture_as_pdf),
+      ),
+      appBar: AppBar(
+        title: Text(invoice.name),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Card(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Customer',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
