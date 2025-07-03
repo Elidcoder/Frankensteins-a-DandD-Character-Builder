cd546f1cc0141db12d2e83c4d851a814b7b972b4
Eli Gilbert <eliaarongilbert@Gmail.com>
2025-07-03 14:54:24 +0100

    feat(create_a_character_pages): create barrel export files to simplify imports

---
+++ lib/pages/create_a_character_pages/creation_tabs.dart
@@ -0,0 +1,6 @@
+export "edit_tabs.dart";
+export "background_tab.dart";
+export "backstory_tab.dart";
+export "race_tab.dart";
+export "ability_score_tab.dart";
+export "basics_tab.dart";
---
+++ lib/pages/create_a_character_pages/edit_tabs.dart
@@ -0,0 +1,3 @@
+export "spells_tab.dart";
+export "asi_feat_tab.dart";
+export "equipment_tab.dart";
