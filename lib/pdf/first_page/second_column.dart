44e7939e8feb1ee99b17b9a58bfcb3b99f0db6fb
Eli Gilbert <eliaarongilbert@Gmail.com>
2025-09-07 00:31:31 +0100

    fix(second_column): add missing dependency import

--- lib/pdf/first_page/second_column.dart
+++ lib/pdf/first_page/second_column.dart
@@ -2,6 +2,7 @@
 import "package:frankenstein/pdf/generator.dart" show formatList;
 import "package:frankenstein/pdf/utils.dart" show modifierFromAbilityScore, decodeBonus, proficiencyBonus, formatNumber, PDF_DARK_GREY, PDF_WHITE, PDF_LIGHT_GREY;
 import "package:pdf/widgets.dart";
+import "../utils.dart";
 
 // Project Import
 import "../../content_classes/all_content_classes.dart";
